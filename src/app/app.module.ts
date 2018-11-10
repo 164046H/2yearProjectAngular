42752d8c1cedf1b7adcfc48f9e235ac513d614a9
164046H <shashinika19961104@gmail.com>
2018-11-10 17:56:57 +0530

    add create event page for trip component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidenavbarComponent } from './SharePart/sidenavbar/sidenavbar.component';
 
 import { AuthServiceService } from "./AuthGards/auth-service.service";
+import { CreateEventPageForTripComponent } from './events/create-event-page-for-trip/create-event-page-for-trip.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { AuthServiceService } from "./AuthGards/auth-service.service";
     //LoggedHeaderComponent,
     LogingHeaderComponent,
     SidenavbarComponent,
+    CreateEventPageForTripComponent,
     
   ],
   imports: [
